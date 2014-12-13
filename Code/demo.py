dea336acc069ea1c43031c64c7904f4b1ebec5e4
Swatisoni <ssvk3@mail.umkc.edu>
2014-12-12 17:01:31 -0800

    demo for spark API

---
+++ Code/demo.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 12 16:47:37 2014
+
+@author: ssoni
+"""
+
+from pyspark import SparkContext
+
+
+if __name__ == "__main__":
+    sc = SparkContext(appName="GibbsSampler")
+    x = sc.parallelize(range(0,3)).keyBy(lambda x: x*x)
+    y = sc.parallelize(zip(range(0,5), range(0,5)))
+    map((lambda (x,y): (x, (list(y[0]), (list(y[1]))))), sorted(x.cogroup(y).collect()))
+    
+    print "Use of KeyBy"
+    z = sc.parallelize(zip(range(0,5), range(0,5), range(0,5), range(6,10), range(11,15))).keyBy(lambda x: x*x)
