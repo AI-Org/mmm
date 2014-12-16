5cf094d6f523f2aea801dc10debfe6d407d9fb49
Swatisoni <ssvk3@mail.umkc.edu>
2014-12-15 21:34:59 -0500

    added functions for get_d_count_grpby_level2 and d_child_counts

--- Code/gibbs_init.py
+++ Code/gibbs_init.py
@@ -41,6 +41,13 @@ def get_d_childcount(obj):
     keyBy_h2_to_h1 = obj.map(lambda (index, hierarchy_level1, hierarchy_level2, week, y1, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13): (hierarchy_level2, hierarchy_level1)).groupByKey()
     return keyBy_h2_to_h1.map(lambda (x,iter): (x, sum(1 for _ in set(iter))))
 
+def get_d_count_grpby_level2(obj):
+    
+    keyBy_h2_week = obj.map(lambda (index, hierarchy_level1, hierarchy_level2, week, y1, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13): (hierarchy_level2, week))
+    return keyBy_h2_week
+    # return keyBy_h2.map(lambda (x,iter): (x, sum(1 for _ in set(iter))))
+
+
 
 def gibbs_init(model_name, source_RDD, hierarchy_level1, hierarchy_level2, p, df1, y_var, x_var_array, coef_means_prior_array, coef_precision_prior_array, sample_size_deflator, initial_vals):
     text_output = 'Done: Gibbs Sampler for model model_name is initialized.  Proceed to run updates of the sampler by using the gibbs() function.  All objects associated with this model are named with a model_name prefix.'
@@ -65,6 +72,12 @@ def gibbs_init_test(sc, d, keyBy_groupby_h2_h1):
     # the following computes the number of stores in each department
     key_pairs = get_d_childcount(d)
     print "d_child_counts are : ", key_pairs.collect()
+    
+    # since the number of weeks of data for each deparment_name-tiers is different.
+    # we wll precompute this quantity for each department_name-tier
+    m1_d_count_grpby_level2 = get_d_count_grpby_level2(d)
+    print "Available data for each department_name-tiers", m1_d_count_grpby_level2.countByKey()
+   
     
    
     
