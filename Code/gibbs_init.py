804f05c3667f73c3fec003f7c1b8a01deb4991fc
Swatisoni <ssvk3@mail.umkc.edu>
2014-12-17 11:49:59 -0800

    For the RDD m1_Vbeta_inv_Sigmabeta_j_draw transformation there is need to first collect and then apply the lamdba function since the sorted cogroup-rdd is a list if collected and then we can apply the lambda of iters on it. This also solves the error 'PipelinedRDD' object is not iterable

--- Code/gibbs_init.py
+++ Code/gibbs_init.py
@@ -30,26 +30,20 @@ def create_x_matrix_y_array(recObj):
     
 def create_xtx_matrix_xty(obj):
     import numpy
-    
     #recObj is of the form of [key, <Iterable of all values tuples>]
     keys = obj[0]
     x_matrix = obj[1] 
     x_matrix_t = numpy.transpose(x_matrix)
-
     xt_x = x_matrix_t * x_matrix
-       
     y_matrix = obj[2]
     xt_y = x_matrix_t * y_matrix
-    
     return (keys, xt_x, xt_y)
 
-def get_d_childcount(obj):
-        
+def get_d_childcount(obj):     
     keyBy_h2_to_h1 = obj.map(lambda (index, hierarchy_level1, hierarchy_level2, week, y1, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13): (hierarchy_level2, hierarchy_level1)).groupByKey()
     return keyBy_h2_to_h1.map(lambda (x,iter): (x, sum(1 for _ in set(iter))))
 
-def get_d_count_grpby_level2(obj):
-    
+def get_d_count_grpby_level2(obj): 
     keyBy_h2_week = obj.map(lambda (index, hierarchy_level1, hierarchy_level2, week, y1, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13): (hierarchy_level2, week))
     return keyBy_h2_week
     # return keyBy_h2.map(lambda (x,iter): (x, sum(1 for _ in set(iter))))
@@ -62,6 +56,7 @@ def get_ols_initialvals_beta_i(obj):
     print('Coefficients: \n', regr.coef_)
     return (obj[3], obj[4], regr.coef_)
 
+
 def get_ols_initialvals_beta_j(obj):
     from sklearn import linear_model
     regr = linear_model.LinearRegression()
@@ -70,18 +65,21 @@ def get_ols_initialvals_beta_j(obj):
     print('Coefficients: \n', regr.coef_)
     #hierarchy_level2 = a matrix obj[3] of same values in hierarchy_level2
     return (obj[3], regr.coef_)
+
     
 def get_random_initialvals_beta_i(obj):
     coeff = gu.initial_vals_random(p_var)
     #hierarchy_level2 = obj[3]
     #hierarchy_level1 = obj[4]
     return (obj[3], obj[4], coeff)
+
     
 def get_random_initialvals_beta_j(obj):
     coeff = gu.initial_vals_random(p_var)
     #hierarchy_level2 = obj[3]
     #hierarchy_level1 = obj[4]
     return (obj[3], coeff) 
+
 
 def create_join_by_h2_only(t1,t2):
     # t1 is a list of ((u'"1"', u'"B8"'), (u'"1"', u'"B8"', array([[  7.16677290e-01,   4.15236265e-01,   7.02316511e-02,
@@ -99,6 +97,7 @@ def create_join_by_h2_only(t1,t2):
                 tup = (hierarchy_level2, hierarchy_level1, values_array_i, values_array_j)
                 joined.append(tup)
     return joined
+
     
 def get_Vbeta_j_mu(obj):
     global accum;
@@ -120,6 +119,7 @@ def get_Vbeta_j_mu(obj):
     # iter, hierarchy_level2, Vbeta_j_mu
     return accum, keys, Vbeta_j_mu 
 
+
 def get_m1_Vbeta_j_mu_pinv(obj):
     import numpy as np
     global df1_var
@@ -131,6 +131,7 @@ def get_m1_Vbeta_j_mu_pinv(obj):
     phi = np.linalg.pinv(gu.matrix_scalarmult_plr(Vbeta_j_mu, df1_var)) 
     Vbeta_inv_j_draw = gu.Vbeta_inv_draw(df1_var, phi)
     return (seq, hierarchy_level2, Vbeta_inv_j_draw)
+
     
 def np_pinv(Vbeta_inv_j_draw, n1, coef_precision_prior_array):
     import numpy as np
@@ -138,55 +139,48 @@ def np_pinv(Vbeta_inv_j_draw, n1, coef_precision_prior_array):
     temp_add = gu.matrix_scalarmult_plr(temp, gu.matrix_diag_plr(coef_precision_prior_array))
     return np.linalg.pinv(temp_add)    
 
+
 def gibbs_init(model_name, source_RDD, hierarchy_level1, hierarchy_level2, p, df1, y_var, x_var_array, coef_means_prior_array, coef_precision_prior_array, sample_size_deflator, initial_vals):
     text_output = 'Done: Gibbs Sampler for model model_name is initialized.  Proceed to run updates of the sampler by using the gibbs() function.  All objects associated with this model are named with a model_name prefix.'
     return text_output
 
 
 def gibbs_init_test(sc, d, keyBy_groupby_h2_h1, initial_vals, p):
-#if __name__ == '__main__':
+    #if __name__ == '__main__':
     global p_var
     global coef_precision_prior_array_var
     # sc = SparkContext(appName="gibbs_init")
     # of the form keys, x_array, y_array
     m1_d_array_agg = keyBy_groupby_h2_h1.map(create_x_matrix_y_array)
-    
     #  we need to make use of X'X and X'y
     m1_d_array_agg_constants = m1_d_array_agg.map(create_xtx_matrix_xty)
-
     #print m1_d_array_agg_constants.take(1)
-
     # Compute the childcount at each hierarchy level
     # computing the number of hierarchy_level2 nodes for each of the hierarchy_level1 node
     # think of h2 as department and h1 as the stores 
     # the following computes the number of stores in each department
     m1_d_childcount = get_d_childcount(d)
     print "d_child_counts are : ", m1_d_childcount.count()
-    
     # since the number of weeks of data for each deparment_name-tiers is different.
     # we wll precompute this quantity for each department_name-tier
     m1_d_count_grpby_level2 = get_d_count_grpby_level2(d)
     print "Available data for each department_name-tiers", m1_d_count_grpby_level2.countByKey()
-
     # structure to compute maps by h2 as key only at m1_d_array_agg levels
-    keyBy_h2 = d.keyBy(lambda (index, hierarchy_level1, hierarchy_level2, week, y1, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13): (hierarchy_level2)).groupByKey().map(create_x_matrix_y_array)
-      
+    keyBy_h2 = d.keyBy(lambda (index, hierarchy_level1, hierarchy_level2, week, y1, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13): (hierarchy_level2)).groupByKey().map(create_x_matrix_y_array) 
     if(initial_vals == "ols"):
-    # Compute OLS estimates for reference
+        # Compute OLS estimates for reference
         m1_ols_beta_i = m1_d_array_agg.map(get_ols_initialvals_beta_i).keyBy(lambda (h2,h1,coff): (h2, h1))
         print "Coefficients for LL after keyby H2", m1_ols_beta_i.count()
-        
+        # similarly we compute the m1_ols_beta_j which uses the RDD mapped upon only hierarchy_level2
         m1_ols_beta_j = keyBy_h2.map(get_ols_initialvals_beta_j).keyBy(lambda (h2,coff): (h2))
         print "Coefficients for LL after keyby H2", m1_ols_beta_j.count()
-    
+    # in case the initial_vals are defined as "random" we compute the exact same 
+    # data structures using deviates from Uniform distribution
     if(initial_vals == "random"):
         print "Draw random array samples of p elements from the uniform(-1,1) dist'n"
         p_var = p
-
-        m1_ols_beta_i = m1_d_array_agg.map(get_random_initialvals_beta_i).keyBy(lambda (h2,h1,coff): (h2, h1))
-        
-        m1_ols_beta_j = keyBy_h2.map(get_random_initialvals_beta_j).keyBy(lambda (h2,coff): (h2))
-        
+        m1_ols_beta_i = m1_d_array_agg.map(get_random_initialvals_beta_i).keyBy(lambda (h2,h1,coff): (h2, h1))        
+        m1_ols_beta_j = keyBy_h2.map(get_random_initialvals_beta_j).keyBy(lambda (h2,coff): (h2))     
     #-- Using the above initial values of the coefficients and drawn values of priors, 
     #   compute initial value of coefficient var-cov matrix (Vbeta_i_mu) 
     #   FOR EACH group i, with group j coefficients as priors, and 
@@ -209,10 +203,11 @@ def gibbs_init_test(sc, d, keyBy_groupby_h2_h1, initial_vals, p):
     #  y[0][1] = h2 from in m1_Vbeta_j_mu_pinv 
     #  y[1][1] = n1 from m1_d_childcount_groupBy_h2, 
     #  y[0][2] = Vbeta_inv_j_draw from m1_Vbeta_j_mu_pinv, np_pin()
-    m1_Vbeta_inv_Sigmabeta_j_draw = map(lambda (x,y): (x, y[0][0], y[0][1], y[1][1] , y[0][2], np_pinv(y[0][2], y[1][1], coef_precision_prior_array_var)), sorted(m1_Vbeta_j_mu_pinv.cogroup(m1_d_childcount_groupBy_h2)))
+    m1_Vbeta_inv_Sigmabeta_j_draw = map(lambda (x,y): (x, y[0][0], y[0][1], y[1][1] , y[0][2], np_pinv(y[0][2], y[1][1], coef_precision_prior_array_var)), sorted(m1_Vbeta_j_mu_pinv.cogroup(m1_d_childcount_groupBy_h2).collect()))
     print "m1_Vbeta_inv_Sigmabeta_j_draw Take 1: ", m1_Vbeta_inv_Sigmabeta_j_draw.take(1)
     print "m1_Vbeta_inv_Sigmabeta_j_draw Count: ", m1_Vbeta_inv_Sigmabeta_j_draw.count()
         
+    
     
     # exp with cogroup 
     join_coefi_coefj = map(lambda (x, y): (x, (list(y[0]), list(y[1]))),
--- Code/gibbs_udfs.py
+++ Code/gibbs_udfs.py
@@ -7,6 +7,7 @@ from scipy.stats import chi2
 
 # Function takes recObj is of the form of [key, <Iterable of all values tuples>]
 # returns matrix from recIter objects and returns the shape of the matrix
+
 def createMatrix(recObj):
     """
        Take an iterable of records, where the key corresponds to a certain age group
@@ -43,6 +44,7 @@ def matrix_scalarmult_plr(matrix, value):
 # beta_mu is anpther float8[] matrix or numpy.array or numpy.matrix
 # according to Koop pp. 157 sigma(Beta_i) is a k-vector containing the sums of the elements of Beta_i
 # hance we convert beta_i_diff to mat before multiplying
+
 def Vbeta_i_mu(beta_i, beta_mu):
     beta_i_diff = np.subtract(beta_i, beta_mu)
     Vbeta_i_mu = np.multiply(beta_i_diff, np.mat(beta_i_diff).transpose())
@@ -54,6 +56,7 @@ def Vbeta_i_mu(beta_i, beta_mu):
 ## phi is the matrix
 ## taken from
 ## https://gist.github.com/jfrelinger/2638485
+
 def wishartrand(nu, phi):
     dim = phi.shape[0]
     chol = cholesky(phi)
@@ -75,6 +78,7 @@ def wishartrand(nu, phi):
 #  phi is Inverse scale matrix (p X p) i.e S (pXp)
 # The mean of a Wishart random variable with v degrees of freedom and inverse scale matrix S is vS.
 # rwish generates one random draw from the distribution
+
 def Vbeta_inv_draw(nu, phi):
     # import a lib MCMCpack and return rwish(arg1,arg2)
     return wishartrand(nu, phi)
@@ -82,6 +86,7 @@ def Vbeta_inv_draw(nu, phi):
 # Function to compute mean pooled coefficient vector to use in drawing a new pooled coefficient vector.  
 # This function allows for user-specified priors on the coefficients.  
 # For use at highest level of the hierarchy.  (7.27) of Koop pp.157.
+
 def beta_mu_prior(arg1, arg2, arg3, arg4, arg5):
     #beta_mu<- arg1%*%(arg2%*%arg3+as.matrix(arg4*arg5))
     mat1 = np.dot(np.mat(arg4),np.mat(arg5))
@@ -93,12 +98,14 @@ def beta_mu_prior(arg1, arg2, arg3, arg4, arg5):
 # beta_draws are samples from mvrnprm or multivariate normal distribution.
 # it relies on MASS library in the original implementation However, we will be using
 # numpy and its random package to perform the same operation
+
 def beta_draw(mean, cov):
     return np.random.multivariate_nprmal(mean, cov, 1)
 
 # Function to compute Vbeta_i, as defined in Equation (7.25) of Koop pp.156.   
 # Only computed at lowest level of the hierarchy (i.e. the level that "mixes" directly with the data, namely X'X).
 # The solve(mat1*%*mat2 + mat3) is finding the inverse of A where A = mat1 %*% mat2 + mat3
+
 def Vbeta_i(mat1, mat2, mat3):
     mat1_r = np.dot(mat1, mat2)
     matr = np.add(mat1_r, mat3)
@@ -106,6 +113,7 @@ def Vbeta_i(mat1, mat2, mat3):
 
 # Function to compute beta_i_mean, as defined in (7.25) of Koop pp.156.
 # Only computed at lowest level of the hierarchy (i.e. the level that "mixes" directly with the data, namely X'y).
+
 def beta_i_mean(mat1, value, mat2, mat3, mat4):
     mat_r1 = np.dot(np.matrix(mat3), np.matrix(mat4))
     mat_r2 = np.dot(value, mat2)
@@ -115,6 +123,7 @@ def beta_i_mean(mat1, value, mat2, mat3, mat4):
 # Function to draw h from gamma dist'n, as defined in (7.28) of Koop pp.157. 
 # rate is an alternate way to specify the scale, shape of the gamma distribution
 # numpy.random.gamma(shape, scale=1.0, size=None) is equivalent to rgamma(n, shape, rate = 1, scale = 1/rate)
+
 def h_draw(m, v):
     shape_g = v/2
     rate_g = v / (2 * m)
@@ -123,6 +132,7 @@ def h_draw(m, v):
      
 # Function to draw random array sample of p elements from the uniform(-1,1) dist'n
 # numpy.random.uniform(low=0.0, high=1.0, size=None)
+     
 def initial_vals_random(p):
     return np.random.uniform(-1,1,p)
 
