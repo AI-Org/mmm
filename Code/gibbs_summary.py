812d0fb03d507b994c49c22b4f4dcdcd0e4c473a
Swatisoni <ssvk3@mail.umkc.edu>
2015-01-16 17:36:34 -0800

    Fixed alias name for gibbs transformation file one more time

--- Code/gibbs_summary.py
+++ Code/gibbs_summary.py
@@ -27,7 +27,7 @@ def m1_summary_geweke_conv_diag_detailed(hierarchy_level1, hierarchy_level2, raw
     geweke_part_40_percent = m1_beta_i_draw_long_keyBy_h2_h1_driver_last_40.map(lambda (x, y): (x, gtr.compute_se_sc_i_avg_sc_i(y, raw_iters, burn_in))).keyBy(lambda (x, y): x)
     
     Joined_geweke_part_10_percent_with_geweke_part_40_percent = geweke_part_10_percent.cogroup(geweke_part_40_percent)
-    m1_summary_geweke_conv_diag_detailed = Joined_geweke_part_10_percent_with_geweke_part_40_percent.map(lambda (x,y): (x, list(y[0])[0][0], list(y[0])[0][1], list(y[1])[0][0], list(y[1])[0][1], str.get_cd_beta_i(list(y[0])[0][0], list(y[0])[0][1], list(y[1])[0][0], list(y[1])[0][1])))
+    m1_summary_geweke_conv_diag_detailed = Joined_geweke_part_10_percent_with_geweke_part_40_percent.map(lambda (x,y): (x, list(y[0])[0][0], list(y[0])[0][1], list(y[1])[0][0], list(y[1])[0][1], gtr.get_cd_beta_i(list(y[0])[0][0], list(y[0])[0][1], list(y[1])[0][0], list(y[1])[0][1])))
     
     
     return m1_summary_geweke_conv_diag_detailed
