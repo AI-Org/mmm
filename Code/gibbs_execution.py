5d8da4f3aa71a9bb2857e12a1ea21dc068183e0f
Swatisoni <ssvk3@mail.umkc.edu>
2015-01-19 21:04:07 -0800

    renamed the data source

--- Code/gibbs_execution.py
+++ Code/gibbs_execution.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     """
     sc = SparkContext(appName="GibbsSampler")
     
-    file = sys.argv[1] if len(sys.argv) > 1 else "hdfs:///data/d.csv"
+    file = sys.argv[1] if len(sys.argv) > 1 else "hdfs:///user/ssoni/data/d.csv"
     ## load all data as separate columns
     d = load(file) 
     keyBy_groupby_h2_h1 = d.keyBy(lambda (index, hierarchy_level1, hierarchy_level2, week, y1, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13): (hierarchy_level2, hierarchy_level1)).groupByKey().cache()     
