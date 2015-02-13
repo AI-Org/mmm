4c90d08e5429352ec50471308359ae279cf2da66
Swatisoni <ssvk3@mail.umkc.edu>
2015-02-13 10:57:37 -0800

    run Summary_stats to get exact summary statistics from the backend code

---
+++ Code/Summary_stats.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 13 10:38:55 2015
+
+@author: ssoni
+"""
+import pickle
+
+if __name__ == "__main__":
+    print "printing the summary statistics"
+    
+    path = '/home/ssoni/mmm_t/Code/result_diag/cd_pct.data'
+    f=open(file, 'rb')  
+    obj_dict = pickle.load(f) 
+    print "cd_signif , denom, cd_pct values"
+    print obj_dict
